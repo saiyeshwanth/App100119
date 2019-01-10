a80a61a9148ba26f9c8bd6aa86d3e3a8d8cbeaa9
yesh <psy029@gmail.com>
2019-01-10 16:39:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/app100119/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.android.app100119
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        signup.setOnClickListener { }
+
+
+    }
+}
